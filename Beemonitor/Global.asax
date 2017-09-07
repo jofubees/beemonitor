22a010e993f931849e6123d25f02183a4bff279c
john <john@JOHN-XPS>
2017-09-07 11:50:46 +0100

    Add project files.

---
+++ Beemonitor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Beemonitor.MvcApplication" Language="C#" %>
---
+++ Beemonitor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Beemonitor.Startup))]
+namespace Beemonitor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
