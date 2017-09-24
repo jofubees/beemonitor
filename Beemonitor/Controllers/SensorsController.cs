3a658d9fb1dc8576ab2f6672a58ace1814fb64ef
johnF <john.futcher@btopenworld.com>
2017-09-24 14:03:53 +0100

    Sensor starting to work

--- Beemonitor/Controllers/SensorsController.cs
+++ Beemonitor/Controllers/SensorsController.cs
@@ -1,33 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using Beemonitor.Migrations;
 using Beemonitor.Models;
-using Beemonitor.ViewModels;
 
 namespace Beemonitor.Controllers
 {
     public class SensorsController : Controller
     {
-        private ApplicationDbContext _context;
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-        public SensorsController()
+        // GET: Sensors
+        public ActionResult Index()
         {
-            _context = new ApplicationDbContext();
+            var sensors = db.Sensors.Include(s => s.SensorType);
+            return View(sensors.ToList());
+        }
+
+        // GET: Sensors/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sensor sensor = db.Sensors.Find(id);
+            if (sensor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sensor);
+        }
+
+        // GET: Sensors/Create
+        public ActionResult Create()
+        {
+            ViewBag.SensorTypeId = new SelectList(db.SensorTypes, "SensorTypeId", "TypeDescription");
+            return View();
+        }
+
+        // POST: Sensors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SensorName,SensorTypeId")] Sensor sensor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sensors.Add(sensor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SensorTypeId = new SelectList(db.SensorTypes, "SensorTypeId", "TypeDescription", sensor.SensorTypeId);
+            return View(sensor);
+        }
+
+        // GET: Sensors/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sensor sensor = db.Sensors.Find(id);
+            if (sensor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SensorTypeId = new SelectList(db.SensorTypes, "SensorTypeId", "TypeDescription", sensor.SensorTypeId);
+            return View(sensor);
+        }
+
+        // POST: Sensors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SensorName,SensorTypeId")] Sensor sensor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sensor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SensorTypeId = new SelectList(db.SensorTypes, "SensorTypeId", "TypeDescription", sensor.SensorTypeId);
+            return View(sensor);
+        }
+
+        // GET: Sensors/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sensor sensor = db.Sensors.Find(id);
+            if (sensor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sensor);
+        }
+
+        // POST: Sensors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Sensor sensor = db.Sensors.Find(id);
+            db.Sensors.Remove(sensor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
         {
-            _context.Dispose();
-        }
-        // GET: Sensors
-        public ActionResult Index()
-        {
-            var sensors = _context.Sensors.ToList();             // set up to retrieve all apiaries in database
-            return View(sensors);                        // and pass that list of apiaries to the view
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
-
 }
